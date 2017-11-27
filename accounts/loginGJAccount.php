c72826a441002918b5ba74fc561094570bbe3b6e
xXFireBlastXx <triplehwwe2013@gmail.com>
2017-11-26 21:14:26 -0600

    Add files via upload

---
+++ accounts/loginGJAccount.php
@@ -0,0 +1,68 @@
+<?php
+include "../incl/lib/connection.php";
+require "../incl/lib/generatePass.php";
+require_once "../incl/lib/exploitPatch.php";
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+	$ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+	$ip = $_SERVER['REMOTE_ADDR'];
+}
+$ep = new exploitPatch();
+//here im getting all the data
+$udid = $ep->remove($_POST["udid"]);
+$userName = $ep->remove($_POST["userName"]);
+$password = $ep->remove($_POST["password"]);
+//registering
+$query = $db->prepare("SELECT accountID FROM accounts WHERE userName LIKE :userName");
+$query->execute([':userName' => $userName]);
+if($query->rowCount() == 0){
+	exit("-1");
+}
+$account = $query->fetch();
+//rate limiting
+$newtime = time() - 3600;
+$query6 = $db->prepare("SELECT count(*) FROM actions WHERE type = '1' AND timestamp > :time AND value2 = :ip");
+$query6->execute([':time' => $newtime, ':ip' => $ip]);
+if($query6->fetchColumn() > 5){
+	exit("-12");
+}
+//authenticating
+$generatePass = new generatePass();
+$pass = $generatePass->isValidUsrname($userName, $password);
+if ($pass == 1) { //success
+	//userID
+	$id = $account["accountID"];
+	$query2 = $db->prepare("SELECT userID FROM users WHERE extID = :id");
+
+	$query2->execute([':id' => $id]);
+	if ($query2->rowCount() > 0) {
+		$userID = $query2->fetchAll()[0]["userID"];
+	} else {
+		$query = $db->prepare("INSERT INTO users (isRegistered, extID, userName)
+		VALUES (1, :id, :userName)");
+
+		$query->execute([':id' => $id, ':userName' => $userName]);
+		$userID = $db->lastInsertId();
+	}
+	//logging
+	$query6 = $db->prepare("INSERT INTO actions (type, value, timestamp, value2) VALUES 
+												('2',:username,:time,:ip)");
+	$query6->execute([':username' => $userName, ':time' => time(), ':ip' => $ip]);
+	//result
+	echo $id.",".$userID;
+	if(!is_numeric($udid)){
+		$query2 = $db->prepare("SELECT userID FROM users WHERE extID = :udid");
+		$query2->execute([':udid' => $udid]);
+		$usrid2 = $query2->fetchColumn();
+		$query2 = $db->prepare("UPDATE levels SET userID = :userID, extID = :extID WHERE userID = :usrid2");
+		$query2->execute([':userID' => $userID, ':extID' => $id, ':usrid2' => $usrid2]);	
+	}
+}else{ //failure
+	echo -1;
+	$query6 = $db->prepare("INSERT INTO actions (type, value, timestamp, value2) VALUES 
+												('1',:username,:time,:ip)");
+	$query6->execute([':username' => $userName, ':time' => time(), ':ip' => $ip]);
+}
+?>
---
+++ config/security.php
@@ -0,0 +1,3 @@
+<?php
+$cloudSaveEncryption = 0; //0 = password string replacement, 1 = cloud save encryption (password dependant)
+?>
