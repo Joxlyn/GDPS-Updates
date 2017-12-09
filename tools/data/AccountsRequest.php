b6671955237fafc6ba1673d3e0d22b83416a311e
xXFireBlastXx <triplehwwe2013@gmail.com>
2017-12-08 20:44:49 -0600

    Updated AccountsRequest.php

--- tools/data/AccountsRequest.php
+++ tools/data/AccountsRequest.php
@@ -1,4 +1,4 @@
 //Aqui es donde se genera el explot de la password de tu cuenta
 <h1>AUN ESTA EN PROGRESO!</h1>
 <a href="../data">Check your accounts and levels of your GDPS here!</a>
-<h1>Nothing is Hidden!</h1/
+<h1>Nothing is Hidden!</h1>
