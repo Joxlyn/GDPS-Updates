65a2a548d423305648ffeaec659258c3be9dea19
xXFireBlastXx <triplehwwe2013@gmail.com>
2017-12-08 21:29:17 -0600

    AccountsRequest.php Now Is Visible For All!

--- tools/data/AccountsRequest.php
+++ tools/data/AccountsRequest.php
@@ -1,4 +1,4 @@
-//Aqui es donde se genera el explot de la password de tu cuenta
+<h1>Ve los niveles y cuentas que existen en tu GDPS!</h1>
 <h1>AUN ESTA EN PROGRESO!</h1>
-<a href="../data">Check your accounts and levels of your GDPS here!</a>
+<a href="/data">Check your accounts and levels of your GDPS here!</a>
 <h1>Nothing is Hidden!</h1>
