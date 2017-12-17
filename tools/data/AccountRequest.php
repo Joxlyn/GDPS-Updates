2d7b5f5f90e7d799cedc1616da3e40b0c362977e
xXFireBlastXx <triplehwwe2013@gmail.com>
2017-12-16 22:52:16 -0600

    Update AccountRequest.php Now is closed

--- tools/data/AccountRequest.php
+++ tools/data/AccountRequest.php
@@ -1,4 +1,1 @@
-<h1>Ve los niveles y cuentas que existen en tu GDPS!</h1
-<h1>AUN ESTA EN PROGRESO!</h1
-<a href="/data">Check your accounts and levels of your GDPS here!</a
 <h1>Nothing is Hidden!</h1>
