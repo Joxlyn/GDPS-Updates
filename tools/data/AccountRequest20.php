7708c22cdc8037af4a025fdc083bf79ee8e87584
xXFireBlastXx <triplehwwe2013@gmail.com>
2017-12-16 22:49:02 -0600

    Added AccountRequest20.php and Update AccountRequest.php

---
+++ tools/data/AccountRequest20.php
@@ -0,0 +1,40 @@
+<h2>This table shows the types of mod that exist in the Geometry Dash 2.11 (Used to create Roles) (16/12/2017)</h2>
+<h1>THIS IS OPTIONAL, YOU CAN CREATE YOUR PERSONALIZED ROLES, THIS IS JUST AN EXAMPLE</h1>
+<html>
+<h1>Account Request for Mod Version 2.0</h1>
+<head><title>Moderators Type</title></head>
+<table>
+<tr>
+  <td><strong>ID</strong></td>
+  <td><strong>Name</strong></td>
+  <td><strong>Type</strong></td>
+  <td><strong>Badge Type</strong></td>
+  <td><strong>Badge Color</strong></td>
+  <td><strong>Comment Color</strong></td>
+  <td><strong>RGB Format</strong></td>
+</tr>
+<tr>
+  <td>1</td>
+  <td>Normal Moderator</td>
+  <td>1</td>
+  <td>1</td>
+  <td>Yellow</td>
+  <td>Green</td>
+  <td>0,255,0</td>
+</tr>
+ 
+<tr>
+  <td>2</td>
+  <td>Elder Moderator</td>
+  <td>2</td>
+  <td>2</td>
+  <td>Red</td>
+  <td>Red</td>
+  <td>255,51,51</td>
+</tr>
+</table>
+<h1>Remember that the comments of colors depends on the color you assign to each role, for example, Normal Moderators = Green in Format R, G, B = 0,255,0</h1>
+<h2>Also with the last update of the php files, the commands like !featured, !daily, !weekly, !delete, etc, will no longer depend on isAdmin</h2>
+</body>
+<img src="https://firepowe.000webhostapp.com/gallery/gj_likesicon_001-ts1500262754.png" name="gj_likesicon_001" alt="gj_likesicon_001" />
+</html>
