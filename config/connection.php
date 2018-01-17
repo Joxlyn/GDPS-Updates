28fb3a2b4769c15108e5e9e6b757503295cef3b4
xXFireBlastXx <triplehwwe2013@gmail.com>
2018-01-17 13:40:21 -0600

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "geometrydash";
+$username = "yourusername";
+$password = "yourpassword";
+$dbname = "yourdbname";
 ?>
