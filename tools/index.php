eaf9b931c4917d186b704bac0a318530462b242e
Joxlyn <triplehwwe2013@gmail.com>
2021-03-25 18:47:51 -0600

    Updated files (Fixed bugs), added tools for better management

---
+++ tools/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>(YOUR NAME) Tools</title>
+		<link rel="stylesheet" href="../tools/style.css"/>
+		</head>
+		</div>	
+		<div class="smain nofooter">
+		<h2>Dashboard</h2>
+<p><a href="../dashboard/">Go to Dashboard</a></p>
+<h2>My Account</h2>
+			<p><a href="account/registerAccount.php">Register Account<a href="account/changePassword.php">Change Password</a><a href="account/changeUsername.php">Change Username</a><a href="linkAcc.php">Link Account</a></p>
+			
+			<h2>Levels</h2>
+			<p><a href="levelReupload.php">Level Reupload</a><a href="levelToGD.php">Level To GD</a><a href="stats/unlisted.php">Unlisted Levels</a><a href="stats/packTable.php">Map Packs</a><a href="stats/reportList.php">Reported Levels</a><a href="stats/suggestList.php">Suggest Levels</a><a href="stats/comments.php">Level Comments</a></p>
+			
+			<h2>Songs</h2>
+			<p><a href="songAdd.php">Song Reupload</a><a href="stats/songList.php">Song List</a></p>
+			
+			<h2>Users</h2>
+			<p><a href="stats/getUserInfo.php">Search Users</a><a href="stats/starGains.php">Star Gains</a><a href="stats/top24h.php">Top 24h</a><a href="stats/topWeek.php">Top Week</a><a href="stats/noLogIn.php">Unused Accounts</a></p>
+			
+			<h2>Other</h2>
+			<p><a href="cron/cron.php">Cron Job</a><a href="stats/serverInfo.php">Server Info</a><a href="vipList.php">Roles</a><a href="addQuests.php">Add Quests (Only Owner)</a></p>
+			
+			<h2>Moderation</h2>
+			<p><a href="stats/modActions.php">Mod Actions</a><a href="leaderboardsBan.php">Leaderboards Ban</a><a href="leaderboardsUnban.php">Leaderboards UnBan</a><a href="packCreate.php">Create Map Pack</a><a href="editPack.php">Edit Map Pack</a><a href="manageComments.php">Manage Comments</a></p>
+			
+		</div>
+
+	</body>
+</html>
