470b3eed916759eede6a070ef334158b85622178
Joxlyn <triplehwwe2013@gmail.com>
2021-05-15 17:11:25 -0500

    Added a new command for legendary levels (ONLY WORKS ON GDPS Editor 2.2 by Blaze)

--- incl/misc/commands.php
+++ incl/misc/commands.php
@@ -67,6 +67,20 @@ class Commands {
 			$query->execute([':value' => "1", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
 			return true;
 		}
+			if(substr($comment,0,10) == '!legendary' AND $gs->checkPermission($accountID, "commandEpic")){
+			$query = $db->prepare("UPDATE levels SET starEpic='2' WHERE levelID=:levelID");
+			$query->execute([':levelID' => $levelID]);
+			$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('4', :value, :levelID, :timestamp, :id)");
+			$query->execute([':value' => "2", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+			return true;
+		}
+			if(substr($comment,0,12) == '!unlegendary' AND $gs->checkPermission($accountID, "commandEpic")){
+			$query = $db->prepare("UPDATE levels SET starEpic='0' WHERE levelID=:levelID");
+			$query->execute([':levelID' => $levelID]);
+			$query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('4', :value, :levelID, :timestamp, :id)");
+			$query->execute([':value' => "0", ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+			return true;
+		}
 		if(substr($comment,0,7) == '!unepic' AND $gs->checkPermission($accountID, "commandUnepic")){
 			$query = $db->prepare("UPDATE levels SET starEpic='0' WHERE levelID=:levelID");
 			$query->execute([':levelID' => $levelID]);
