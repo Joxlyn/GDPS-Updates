3b199160e13a723023d19399d8a5a602add23cbd
Joxlyn <triplehwwe2013@gmail.com>
2021-05-31 15:06:53 -0500

    Modified style, code cleanup.

--- tools/stats/unlisted.php
+++ tools/stats/unlisted.php
@@ -1,3 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Unlisted Levels</title>
+		<link rel="stylesheet" href="../style.css"/>
+	</head>
+	
+	<body>
+		
+		
+		<div class="smain">
 <?php
 include "../../incl/lib/connection.php";
 require "../../incl/lib/generatePass.php";
@@ -12,23 +23,29 @@ if(!empty($_POST["userName"]) AND !empty($_POST["password"])){
 		$query = $db->prepare("SELECT accountID FROM accounts WHERE userName=:userName");	
 		$query->execute([':userName' => $userName]);
 		if($query->rowCount()==0){
-			echo "Invalid password or nonexistant account. <a href='unlisted.php'>Try again</a>";
+			echo "<p>Invalid password or nonexistant account</p><a href=''>Try again</a>";
 		}else{
 			$accountID = $query->fetchColumn();
 			$query = $db->prepare("SELECT levelID, levelName FROM levels WHERE extID=:extID AND unlisted=1");	
 			$query->execute([':extID' => $accountID]);
 			$result = $query->fetchAll();
-			echo '<table border="1"><tr><th>ID</th><th>Name</th></tr>';
+			echo '<table><tr><th>ID</th><th>Name</th></tr>';
 			foreach($result as &$level){
 				echo "<tr><td>".$level["levelID"]."</td><td>".$level["levelName"]."</td></tr>";
 			}
 			echo "</table>";
 		}
 	}else{
-		echo "Invalid password or nonexistant account. <a href='unlisted.php'>Try again</a>";
+		echo "<p>Invalid password or nonexistant account</p><a href=''>Try again</a>";
 	}
 }else{
-	echo '<form action="unlisted.php" method="post">Username: <input type="text" name="userName">
-		<br>Password: <input type="password" name="password"><br><input type="submit" value="Show Unlisted Levels"></form>';
+	echo '<form action="" method="post">
+			<input class="smain" type="text" placeholder="Username" name="userName"><br>
+			<input class="smain" type="password" placeholder="Password" name="password"><br>
+			<input class="smain" type="submit" value="Show Levels">
+		</form>';
 }
 ?>
+		</div>
+	</body>
+</html>
