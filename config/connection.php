7ae5577640f8870185cca13ebca911cf1d9fff50
Joxlyn <triplehwwe2013@gmail.com>
2021-11-19 20:48:55 -0600

    Added the option to disable certain functions of the GDPS

--- config/connection.php
+++ config/connection.php
@@ -3,5 +3,20 @@ $servername = "localhost";
 $username = "";
 $password = "";
 $dbname = "";
-$maintenanceMode = 0; #1 = Servers off; 0 = Servers powered on
+
+#GDPS Stuff
+$maintenanceModeALL = 1; #1 = Servers ALL off; 0 = Servers ALL powered on
+$maintenanceModeGETACCCOMMENTS = 0; #1 = Servers get AccComments off; 0 = Servers get AccComments powered on
+$maintenanceModeGETCOMMENTS = 0; #1 = Servers get Comments off; 0 = Servers get Comments powered on
+$maintenanceModeGETGAUNTLETS = 0; #1 = Servers get gauntlets off; 0 = Servers get gauntlets powered on
+$maintenanceModeGETMAPPACKS = 0; #1 = Servers get map packs off; 0 = Servers get map packs powered on
+$maintenanceModeGETDAILYLEVELS = 0; #1 = Servers get daily Levels off; 0 = Servers get daily Levels powered on
+$maintenanceModeGETLEVELS = 0; #1 = Servers get Levels off; 0 = Servers get Levels powered on
+$maintenanceModeGETMESSAGES = 0; #1 = Servers get Messages off; 0 = Servers get Messages powered on
+$maintenanceModeGETUSERINFO = 0; #1 = Servers get User info off; 0 = Servers get User info powered on
+$maintenanceModeQUESTS = 0; #1 = Servers Quests off; 0 = Servers Quests powered on
+$maintenanceModeTOPSCORE = 0; #1 = Servers Top leaderboards off; 0 = Servers Top leaderboards powered on
+
+#Site Stuff
+$maintenanceModeGETSTATUS = 1; #1 = Servers getStatus off; 0 = Servers getStatus powered on
 ?>
