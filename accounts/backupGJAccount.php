56049c3d1b35ca92dd5309eec69a3e13ee8da31d
Joxlyn <triplehwwe2013@gmail.com>
2022-07-20 17:13:07 -0500

    Updated to the latest version of Cvolton files (w/Joxlyn Features)

--- accounts/backupGJAccount.php
+++ accounts/backupGJAccount.php
@@ -8,17 +8,30 @@ include "../config/security.php";
 include "../incl/lib/connection.php";
 require "../incl/lib/generatePass.php";
 require_once "../incl/lib/exploitPatch.php";
-include_once "../incl/lib/defuse-crypto.phar";
+/*include_once "../incl/lib/defuse-crypto.phar";
 use Defuse\Crypto\KeyProtectedByPassword;
 use Defuse\Crypto\Crypto;
-use Defuse\Crypto\Key;
-$ep = new exploitPatch();
+use Defuse\Crypto\Key;*/
 //here im getting all the data
-$userName = $ep->remove($_POST["userName"]);
-$password = $_POST["password"];
-$saveData = $ep->remove($_POST["saveData"]);
-$generatePass = new generatePass();
-$pass = $generatePass->isValidUsrname($userName, $password);
+$userName = ExploitPatch::remove($_POST["userName"]);
+$password = !empty($_POST["password"]) ? $_POST["password"] : "";
+$saveData = ExploitPatch::remove($_POST["saveData"]);
+
+if(empty($_POST["accountID"])) {
+	$query = $db->prepare("SELECT accountID FROM accounts WHERE userName = :userName");
+	$query->execute([':userName' => $userName]);
+	$accountID = $query->fetchColumn();
+} else {
+	$accountID = ExploitPatch::remove($_POST["accountID"]);
+}
+
+if(!is_numeric($accountID)){
+	exit("-1");
+}
+
+$pass = 0;
+if(!empty($_POST["password"])) $pass = GeneratePass::isValid($accountID, $_POST["password"]);
+elseif(!empty($_POST["gjp2"])) $pass = GeneratePass::isGJP2Valid($accountID, $_POST["gjp2"]);
 if ($pass == 1) {
 	$saveDataArr = explode(";",$saveData); //splitting ccgamemanager and cclocallevels
 	$saveData = str_replace("-","+",$saveDataArr[0]); //decoding
@@ -31,31 +44,25 @@ if ($pass == 1) {
 	$lvls = explode("</s><k>4</k><s>",$lvls)[1];
 	$lvls = explode("</s>",$lvls)[0];
 	$protected_key_encoded = "";
-	if($cloudSaveEncryption == 0){
-		$saveData = str_replace("<k>GJA_002</k><s>".$password."</s>", "<k>GJA_002</k><s>not the actual password</s>", $saveData); //replacing pass
+	//if($cloudSaveEncryption == 0){
+		$saveData = str_replace("<k>GJA_002</k><s>".$password."</s>", "<k>GJA_002</k><s>password</s>", $saveData); //replacing pass
 		//file_put_contents($userName, $saveData);
 		$saveData = gzencode($saveData); //encoding back
 		$saveData = base64_encode($saveData);
 		$saveData = str_replace("+","-",$saveData);
 		$saveData = str_replace("/","_",$saveData);
 		$saveData = $saveData . ";" . $saveDataArr[1]; //merging ccgamemanager and cclocallevels
-	}else if($cloudSaveEncryption == 1){
-		$saveData = $ep->remove($_POST["saveData"]);
+	/*}else if($cloudSaveEncryption == 1){
+		$saveData = ExploitPatch::remove($_POST["saveData"]);
 		$protected_key = KeyProtectedByPassword::createRandomPasswordProtectedKey($password);
 		$protected_key_encoded = $protected_key->saveToAsciiSafeString();
 		$user_key = $protected_key->unlockKey($password);
 		$saveData = Crypto::encrypt($saveData, $user_key);
-	}
+	}*/
 	//$query = $db->prepare("UPDATE `accounts` SET `saveData` = :saveData WHERE userName = :userName");
 	//$query->execute([':saveData' => $saveData, ':userName' => $userName]);
-	$query = $db->prepare("SELECT accountID FROM accounts WHERE userName = :userName");
-	$query->execute([':userName' => $userName]);
-	$accountID = $query->fetchColumn();
-	if(!is_numeric($accountID)){
-		exit("-1");
-	}
 	file_put_contents("../data/accounts/$accountID",$saveData);
-	file_put_contents("../data/accounts/keys/$accountID",$protected_key_encoded);
+	file_put_contents("../data/accounts/keys/$accountID","");
 	$query = $db->prepare("SELECT extID FROM users WHERE userName = :userName LIMIT 1");
 	$query->execute([':userName' => $userName]);
 	$result = $query->fetchAll();
--- accounts/registerGJAccount.php
+++ accounts/registerGJAccount.php
@@ -1,13 +1,22 @@
 <?php
+include "../config/security.php";
 include "../incl/lib/connection.php";
 require_once "../incl/lib/exploitPatch.php";
-$ep = new exploitPatch();
+require "../incl/lib/generatePass.php";
+
+if(!isset($preactivateAccounts)){
+	$preactivateAccounts = true;
+}
+
 if($_POST["userName"] != ""){
 	//here im getting all the data
-	$userName = $ep->remove($_POST["userName"]);
-	$password = $ep->remove($_POST["password"]);
-	$email = $ep->remove($_POST["email"]);
+	$userName = ExploitPatch::remove($_POST["userName"]);
+	$password = ExploitPatch::remove($_POST["password"]);
+	$email = ExploitPatch::remove($_POST["email"]);
 	$secret = "";
+	//checking if username is within the GD length limit
+	if(strlen($userName) > 20)
+		exit("-4");
 	//checking if name is taken
 	$query2 = $db->prepare("SELECT count(*) FROM accounts WHERE userName LIKE :userName");
 	$query2->execute([':userName' => $userName]);
@@ -16,9 +25,10 @@ if($_POST["userName"] != ""){
 		echo "-2";
 	}else{
 		$hashpass = password_hash($password, PASSWORD_DEFAULT);
-		$query = $db->prepare("INSERT INTO accounts (userName, password, email, secret, saveData, registerDate, saveKey)
-		VALUES (:userName, :password, :email, :secret, '', :time, '')");
-		$query->execute([':userName' => $userName, ':password' => $hashpass, ':email' => $email, ':secret' => $secret, ':time' => time()]);
+		$gjp2 = GeneratePass::GJP2hash($password);
+		$query = $db->prepare("INSERT INTO accounts (userName, password, email, registerDate, isActive, gjp2)
+		VALUES (:userName, :password, :email, :time, :isActive, :gjp)");
+		$query->execute([':userName' => $userName, ':password' => $hashpass, ':email' => $email, ':time' => time(), ':isActive' => $preactivateAccounts ? 1 : 0, ':gjp' => $gjp2]);
 		echo "1";
 	}
 }
